1d9ed2bab0cb329ab47fc2b1f639b55ff6e9fca4
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-25 15:45:54 -0400

    Successful Angular and SpringBoot connection

--- backend/src/main/java/org/revature/AngularSpringBoot/Controller/EmployeeController.java
+++ backend/src/main/java/org/revature/AngularSpringBoot/Controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.revature.AngularSpringBoot.Model.Employee;
 import org.revature.AngularSpringBoot.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 //@RestController is a convenience annotation that is itself annotated with @Controller and @ResponseBody. Applied to a
 //class to mark it as a request handler. Used to created RESTful web services using Spring MVC.
 @RestController
+
+@CrossOrigin(origins = "http://localhost:4200")
 
 //@RequestMapping is the most common and widely used annotation in Spring MVC. Used to map web requests onto specific handler classes and/or handler methods. 
 //Can be applied to the controller class as well as methods. 
