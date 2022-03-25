4d8a30e16b97bb89c18bbf7cd49155fedc980e60
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-24 20:14:48 -0400

    Unable to retrieve data, perpetual postgres issues

--- angular-frontend/src/app/employee-list/employee-list.component.ts
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee'
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,21 +11,15 @@ export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [{
-      "id": 1,
-      "firstName": "Ronald",
-      "lastName": "Lemus",
-      "emailId": "ronald.cristobal.lemus@outlook.com"
-    },
-    {
-      "id": 2,
-      "firstName": "John",
-      "lastName": "Doe",
-      "emailId": "John@mail.com"
-    }];
+    this.getEmployees();
   }
 
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
+  }
 }
