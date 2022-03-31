722ab012a87a9c0fce50667215e4f6e0903f55c2
phamthinh14 <thinhpham0717@gmail.com>
2022-03-30 20:54:28 -0400

    Added docker-compose and docker file

--- backend/src/test/java/org/revature/AngularSpringBoot/AngularSpringBootApplicationTests.java
+++ backend/src/test/java/org/revature/AngularSpringBoot/AngularSpringBootApplicationTests.java
@@ -2,7 +2,9 @@ package org.revature.AngularSpringBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AngularSpringBootApplicationTests {
 
